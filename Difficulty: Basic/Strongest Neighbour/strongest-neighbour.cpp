654eb7c906044692e50b59333c8d9ef78dd407eb
Heet Mankad <heetmankad06@gmail.com>
2026-08-03 05:58:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Strongest Neighbour/strongest-neighbour.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    vector<int> maxAdj(vector<int> &arr) {
+        // code here
+        vector<int> ans;
+        for(int i=0; i<arr.size()-1; i++)
+        {
+            ans.push_back(max(arr[i],arr[i+1]));
+        }
+        return ans;
+    }
+};
